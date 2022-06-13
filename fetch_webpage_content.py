cb6926727d7152d078b867399383017989d57227
Silver Fan <silverfankw@gmail.com>
2022-06-14 00:14:31 +0800

    complete kmb list

--- fetch_webpage_content.py
+++ fetch_webpage_content.py
@@ -2,18 +2,22 @@ import requests
 import mapper
 from bs4 import BeautifulSoup
 
-selection = "V6X"
+selection = "ATE"
 
 def handle_content(ctx):
 	soup = BeautifulSoup(ctx, 'html.parser')
 
 	# targets = soup.prettify()
-	# targets = soup.find_all(href=mapper.regex["fleet"])
-	targets = soup.find_all(string=mapper.regex["plate"])
+	fleets = soup.find_all(href=mapper.regex["fleet"])
+	plates = soup.find_all(string=mapper.regex["plate"])
 
 	# print(targets)
-	for target in targets:
-		print(target.get_text())
+	for fleet in fleets:
+		print(f'{fleet.get_text()}')
+	for plate in plates:
+		print(f'{plate.get_text()}')
+
+
 
 headers = {'Cookie': 'l=0; hints=1; vid=8351569'}
 response = requests.get(f'http://1005.idv.hk/index.php?page=11&p={mapper.type[selection]}', headers=headers)
